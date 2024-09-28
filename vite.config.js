a5abfb83dd805ae86c6e148db3d88db14d825891
larryp89 <42215520+larryp89@users.noreply.github.com>
2024-09-28 09:01:12 +0100

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/battleships/", // Set this to your repository name
+  base: "/battleships/", // Set this repository name
 });
